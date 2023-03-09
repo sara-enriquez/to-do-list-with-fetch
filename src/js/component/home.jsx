b7c3c94e941c222a383348b273bf27c11051aba7
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-03-09 20:55:04 +0000

    created component with fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const URL = "https://assets.breatheco.de/apis/fake/todos/user/sara-enriquez";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [state, setState] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+
+  const getData = async () =>{
+	try {
+		const response = await fetch(URL, { method: "GET" });
+		const data = await response.json();
+		setState(data);
+		console.log(data);
+		setLoad(true);
+	  } catch (err) {
+		console.log("err");
+	  }
+  }
+  useEffect(() => {
+  getData()
+  }, []);
+
+
+  const addNewTask = async () => {
+  try {
+	const data =[...state, {label: newTask, done: false}]
+	const response = await fetch(URL, { method: "PUT", body: JSON.stringify(data), headers:{"Content-Type": "application/json"}});
+	console.log(response);
+	getData();
+} catch (err) {
+	console.log("err");
+  }
+}
+
+  const inputValue = (e) => {
+     setNewTask(e.target.value)
+  }
+
+  console.log(load);
+  return (
+	<div>
+		<input onChange={inputValue} type="text" placeholder="Add New Task"></input>
+		<button onClick={addNewTask}>Add New Task</button>
+		{load ? state.map((task)=>{
+			return <h1>{task.label}</h1>
+		}):<h1>loading</h1>}
+	</div>
+  );
 };
 
 export default Home;
