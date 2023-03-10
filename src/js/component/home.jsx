fec821b27fe24c2535ed7520d9301de8382ff4f3
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-03-10 23:33:52 +0000

    created: styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,51 +3,117 @@ import React, { useEffect, useState } from "react";
 const URL = "https://assets.breatheco.de/apis/fake/todos/user/sara-enriquez";
 
 const Home = () => {
-  const [state, setState] = useState([]);
+  const [taskList, setTaskList] = useState([]);
   const [load, setLoad] = useState(false);
   const [newTask, setNewTask] = useState("");
 
+  const getData = async () => {
+    try {
+      const response = await fetch(URL, { method: "GET" });
+      const data = await response.json();
+      setTaskList(data);
+      console.log("este es el getData ", data);
+      setLoad(true);
+    } catch (err) {
+      console.log("err");
+    }
+  };
 
-  const getData = async () =>{
-	try {
-		const response = await fetch(URL, { method: "GET" });
-		const data = await response.json();
-		setState(data);
-		console.log(data);
-		setLoad(true);
-	  } catch (err) {
-		console.log("err");
-	  }
-  }
   useEffect(() => {
-  getData()
+    getData();
+    console.log("este es el useEffect del principio", getData());
   }, []);
 
-
   const addNewTask = async () => {
-  try {
-	const data =[...state, {label: newTask, done: false}]
-	const response = await fetch(URL, { method: "PUT", body: JSON.stringify(data), headers:{"Content-Type": "application/json"}});
-	console.log(response);
-	getData();
-} catch (err) {
-	console.log("err");
-  }
-}
+    try {
+      const data = [...taskList, { label: newTask, done: false }];
+      const response = await fetch(URL, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      });
+      console.log("este es el response de addNewTask ", response);
+      console.log("este es el data de addNewTask ", data);
+      getData();
+      input.value = "";
+    } catch (err) {
+      console.log("err");
+    }
+  };
 
   const inputValue = (e) => {
-     setNewTask(e.target.value)
-  }
+    setNewTask(e.target.value);
+  };
 
-  console.log(load);
+  const deleteTask = async (i) => {
+    let newTaskList = [...taskList];
+    newTaskList.splice(i, 1);
+    console.log("este es el newTaskList de deleteTask ", newTaskList);
+    const response = await fetch(URL, {
+      method: "PUT",
+      body: JSON.stringify(newTaskList),
+      headers: { "Content-Type": "application/json" },
+    });
+    getData();
+    console.log("esta es la response de deleteTask ", response);
+  };
+
+  const doneTask = async (i) => {
+    let newTaskList = [...taskList];
+    newTaskList[i].done = !newTaskList[i].done;
+    console.log("este es el newTaskList de doneTask ", newTaskList);
+    const response = await fetch(URL, {
+      method: "PUT",
+      body: JSON.stringify(newTaskList),
+      headers: { "Content-Type": "application/json" },
+    });
+    getData();
+    console.log("esta es la response de doneTask ", response);
+  };
+
   return (
-	<div>
-		<input onChange={inputValue} type="text" placeholder="Add New Task"></input>
-		<button onClick={addNewTask}>Add New Task</button>
-		{load ? state.map((task)=>{
-			return <h1>{task.label}</h1>
-		}):<h1>loading</h1>}
-	</div>
+    <div className="content-home container-fluid">
+      <h1 className="title col-sm-md-lg">Lista de Tareas Pendientes :</h1>
+      <div className="content-form input-group col-sm-md-lg">
+        <input
+          onChange={inputValue}
+          type="text"
+          placeholder="Escribir nueva tarea"
+          id="input"
+          className="input-task form-control col-sm-md-lg"
+        ></input>
+        <button className="btn btn-outline-secondary col-sm-md-lg" onClick={addNewTask}>
+          Añadir
+        </button>
+      </div>
+      <ul className="list col-sm-md-lg">
+        {load ? (
+          taskList.map((task, i) => {
+            return (
+              <li className={task.done ? "li-done" : "li-undone"}>
+                {task.label}
+                <i
+                  onClick={() => deleteTask(i)}
+                  className="fa-regular fa-trash-can"
+                ></i>
+                <i
+                  onClick={() => doneTask(i)}
+                  className={
+                    task.done
+                      ? "fa-solid fa-circle-check"
+                      : "fa-regular fa-circle-check"
+                  }
+                ></i>
+              </li>
+            );
+          })
+        ) : (
+          <h1 className="loading">
+            Loading <i class="fa-solid fa-spinner"></i>
+          </h1>
+        )}
+      </ul>
+    </div>
   );
 };
 
